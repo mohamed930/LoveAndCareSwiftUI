27e5dbaf1251eb846b9618609670f2b6e4e6f527
mohamed930 <mohamed@gmail.com>
2024-05-11 22:49:25 +0300

    Initial Commit

---
+++ LoveAndCare/LoveAndCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveAndCareApp.swift
+//  LoveAndCare
+//
+//  Created by Mohamed Ali on 11/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoveAndCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
